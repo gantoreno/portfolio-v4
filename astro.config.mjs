6bb31fca6aefc0402ca954075066b72150dd19e9
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-09-21 12:04:11 -0400

    Fix edge runtime declaration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercelEdge from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://gantoreno.com",
   output: "server",
-  adapter: vercelEdge(),
+  adapter: vercel({ edgeMiddleware: true }),
   integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
