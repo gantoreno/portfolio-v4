7a2db484fe064a7bc67d0ddb0e22418c60ee1c3f
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-09-22 15:37:35 -0400

    Revert "Test out new optimized animations"
    This reverts commit 11e3c1a1669e9076f92bacfb70413df0eeed6f4b.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 import remarkMath from "remark-math";
 import rehypeMathjax from "rehype-mathjax";
-import rehypeBuildAnimations from "./plugins/rehype-build-animations.mjs";
-import rehypeConvertFigures from "./plugins/rehype-convert-figures.mjs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +17,7 @@ export default defineConfig({
   markdown: {
     remarkRehype: { footnoteLabel: "Reference" },
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeMathjax, rehypeConvertFigures, rehypeBuildAnimations],
+    rehypePlugins: [rehypeMathjax],
     shikiConfig: {
       theme: "css-variables",
       wrap: true,
