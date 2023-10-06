1732bac82e4a8c470df65c20d64a21d56b878256
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-10-06 13:02:49 -0400

    Use astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 import remarkMath from "remark-math";
 import rehypeMathjax from "rehype-mathjax";
@@ -12,15 +13,19 @@ import rehypeMathjax from "rehype-mathjax";
 export default defineConfig({
   site: "https://gantoreno.com",
   output: "server",
-  adapter: vercel({ edgeMiddleware: true }),
-  integrations: [mdx(), tailwind(), sitemap()],
+  adapter: vercel({
+    edgeMiddleware: true
+  }),
+  integrations: [mdx(), tailwind(), sitemap(), compress()],
   markdown: {
-    remarkRehype: { footnoteLabel: "Reference" },
+    remarkRehype: {
+      footnoteLabel: "Reference"
+    },
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
     shikiConfig: {
       theme: "css-variables",
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
