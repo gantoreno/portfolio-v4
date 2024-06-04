75b37a6affd946fafad7666cbd1785f942b7289a
Gabriel Moreno <gmoreno3@apple.com>
2024-06-04 10:44:18 -0400

    Remove seeding

--- db/seed.ts
+++ db/seed.ts
@@ -1,12 +1,3 @@
-import { db, Analytics } from "astro:db";
-
-import { getCollection } from "astro:content";
-
 export default async function seed() {
-  const analytics = (await getCollection("blog")).map((post) => ({
-    slug: post.slug,
-    views: 0,
-  }));
-
-  await db.insert(Analytics).values(analytics);
+  // TODO
 }
